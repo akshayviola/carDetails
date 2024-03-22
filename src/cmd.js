4cea05de9cece7974d4ba09a86fec21f69753a85
akshayviola <akshayviola201@gmail.com>
2024-03-22 11:21:20 +0530

    comments added

--- src/cmd.js
+++ src/cmd.js
@@ -1,17 +1,22 @@
+// THis code provides command line interface for users to input data interactively., primarily used for filtering data based on certian criteria 
+
+// interface for reading data from a readable stream one line at a time
 import readline from "readline";
 
+// method used to create an instance of the readline interface 
+// it takes an object as an argument with input  and output properties in this case standard input and standard output
 const rl = readline.createInterface({
-  input: process.stdin,
+  input: process.stdin, 
   output: process.stdout,
 });
-
+// Returns a Promise that resolves with the user's input after asking the user to enter a value fro the specified key 
 const promptForValue = (key) =>
   new Promise((resolve) =>
     rl.question(`Enter value of ${key} to filter: `, (answer) =>
       resolve(answer.trim() || undefined)
     )
   );
-
+// prompts user to enter values specified in the structure object , the input values are stored in the the result object and returned once all values are enterd 
 export const enterValues = async () => {
   const structure = {
     name: "string",
@@ -28,7 +33,8 @@ export const enterValues = async () => {
   }
   return result;
 };
-
+// It returns a promise that resolves to true if the user enters "yes" or "y" and resolve to false if user enterd "no" or "n"
+// calls an error if the user provide any other input 
 export const promptForYesOrNo = () =>
   new Promise((resolve) => {
     rl.question("Get Filtered Date yes or no: ", (answer) => {
--- src/filters.js
+++ src/filters.js
@@ -1,5 +1,5 @@
 import { filterByParams } from "./helpers.js";
-
+// The function filterData takes tow parameters, data : the data needs to be filterd , filtersKey : its optional represents the key or keys based on which the filtering will be performed .if not provided the function returns the original data without filtering 
 const filterData = (data, filtersKey = undefined) => {
   if (!filtersKey) return data;
   const result = filterByParams(data, filtersKey);
